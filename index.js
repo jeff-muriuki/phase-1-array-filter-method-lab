1b4ae8544302aa73c10be80952b12b214b750d86
jeff muriuki <jeff.muriuki@studet.moringaschool.com>
2024-03-27 22:04:28 +0300

    filter

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+findMatching()
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+fuzzyMatch()
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
+matchName()
